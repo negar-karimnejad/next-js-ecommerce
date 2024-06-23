aa89c867105b531f2780c66bc732c073874d5a7e
negar <negar.karimnejad@gmail.com>
2024-06-23 14:51:30 +0330

    create admin nav

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,18 @@
+import { Nav, NavLink } from "@/components/Nav";
+import React from "react";
+
+function AdminLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <Nav>
+        <NavLink href="/admin">Dashboard</NavLink>
+        <NavLink href="/admin/products">Products</NavLink>
+        <NavLink href="/admin/users">Customers</NavLink>
+        <NavLink href="/admin/orders">Sales</NavLink>
+      </Nav>
+      <div className="container my-6">{children}</div>
+    </div>
+  );
+}
+
+export default AdminLayout;
